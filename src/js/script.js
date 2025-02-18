9cc5d48f3b2c0548733a116b73602973debb1145
Eugene <cigan-yashka@yandex.ru>
2025-02-18 18:54:48 +0300

    fix: add animate on planet and buble

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,51 @@
+// Глобус следит за мышкой.
+
 document.addEventListener('mousemove', event => {
-	const globe = document.querySelector('.about__globe-img')
-	const rect = globe.getBoundingClientRect()
-	const globeX = rect.left + rect.width / 2 // Центр глобуса по X
-	const globeY = rect.top + rect.height / 2 // Центр глобуса по Y
-	const deltaX = event.clientX - globeX
-	const deltaY = event.clientY - globeY
+	const globus = document.querySelector('.about__globe-img')
+	const globusSize = globus.getBoundingClientRect()
+	const globusX = globusSize.left + globusSize.width / 2 // Центр глобуса по X
+	const globusY = globusSize.top + globusSize.height / 2 // Центр глобуса по Y
+	const deltaX = event.clientX - globusX
+	const deltaY = event.clientY - globusY
 	let angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI) + 90 // Вычисляем угол
 
-	globe.style.transform = `translateX(-50%) rotate(${angle}deg)`
+	globus.style.transform = `translateX(-50%) rotate(${angle}deg)`
+})
+
+// Анимация шариков.
+
+function randomPosition() {
+	return Math.random() * 10 - 2
+}
+
+function shakeBubbles() {
+	const bubbles = document.querySelectorAll('.bubble')
+
+	bubbles.forEach(bubble => {
+		const offsetX = randomPosition()
+		const offsetY = randomPosition()
+
+		bubble.style.transition = 'transform 0.5s'
+		bubble.style.transform = `translate(${offsetX}px, ${offsetY}px)`
+	})
+}
+setInterval(shakeBubbles, 100)
+
+// Планета следит за мышкой.
+
+document.addEventListener('mousemove', event => {
+	const planet = document.querySelector('.planet-two') // Меняем на planet-two
+	const orbit = document.querySelector('.orbit-two')
+
+	const orbitSize = orbit.getBoundingClientRect()
+	const orbitCenterX = orbitSize.left + orbitSize.width / 2
+	const orbitCenterY = orbitSize.top + orbitSize.height / 2
+
+	const mouseX = event.clientX
+	const mouseY = event.clientY
+
+	const angle = Math.atan2(mouseY - orbitCenterY, mouseX - orbitCenterX)
+	const angleDeg = angle * (180 / Math.PI)
+
+	planet.style.transform = `translate(-50%, -50%) rotate(${angleDeg}deg) translateX(110px) rotate(-171deg)`
 })
